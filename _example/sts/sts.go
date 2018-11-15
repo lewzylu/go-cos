ae9fac009aa9c6a2b61816f0384c14e08ade026b
toranger <18004535090@163.com>
2018-11-15 19:58:50 +0800

    Add SetCredential interface  (#9)
    * add sts example demo
    
    * add sts demo
    
    * change credential. sts

--- _example/sts/sts.go
+++ _example/sts/sts.go
@@ -1,64 +1,70 @@
 package main
 
 import (
-	"fmt"
 	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/QcloudApi/qcloud_sign_golang"
+	"github.com/lewzylu/go-cos"
+	"github.com/lewzylu/go-cos/debug"
+	"net/http"
 	"net/url"
 	"strings"
-	"net/http"
-	"encoding/json"
- 	"github.com/lewzylu/go-cos"
-        "github.com/lewzylu/go-cos/debug"
-	"github.com/QcloudApi/qcloud_sign_golang"
-
 )
-type Credent struct{
-        SessionToken string `json:"sessionToken"`
-        TmpSecretId  string `json:"tmpSecretId"`
-        TmpSecretKey string `json:"tmpSecretKey"`
-}
-type Data struct{
-        Credentials Credent `json:"credentials`
-                
-}
-type Response struct{
-        Dat Data `json:"data"`
-}
-func main() {
-        // 替换实际的 SecretId 和 SecretKey
-        secretId := ""
-        secretKey := ""
-    
-        // 配置
-        config := map[string]interface{} {"secretId" : secretId, "secretKey" : secretKey, "debug" : false}
-    
-        // 请求参数
-        params := map[string]interface{} {"Region" : "gz", "Action" : "GetFederationToken","name":"alantong","policy":"{\"statement\": [{\"action\": [\"name/cos:GetObject\",\"name/cos:PutObject\"],\"effect\": \"allow\",\"resource\":[\"qcs::cos:ap-guangzhou:uid/1251668577:prefix//1251668577/alantest/*\"]}],\"version\": \"2.0\"}"   }
-    
-        // 发送请求
-        retData, err := QcloudApi.SendRequest("sts", params, config)
-        if err != nil{
-            fmt.Print("Error.", err)
-            return
-        }
-        r := &Response{}
-        err = json.Unmarshal([]byte(retData), r)
-        if err != nil {
-            fmt.Println(err);
-            return
-        }
-        //获取临时ak、sk、token
-        tmp_secId := r.Dat.Credentials.TmpSecretId
-        tmp_secKey := r.Dat.Credentials.TmpSecretKey
-        token := r.Dat.Credentials.SessionToken
 
-    	//fmt.Println("token:", token)
-	u, _ := url.Parse("https://alangz-1251668577.cos.ap-guangzhou.myqcloud.com")
+// Use Qcloud api github.com/QcloudApi/qcloud_sign_golang
+// doc https://cloud.tencent.com/document/product/436/14048
+type Credent struct {
+	SessionToken string `json:"sessionToken"`
+	TmpSecretID  string `json:"tmpSecretId"`
+	TmpSecretKey string `json:"tmpSecretKey"`
+}
+
+// Data data in sts response body
+type Data struct {
+	Credentials Credent `json:"credentials"`
+}
+
+// Response sts response body
+// In qcloud_sign_golang this response only return ak, sk and token
+type Response struct {
+	Dat Data `json:"data"`
+}
+
+func main() {
+	// 替换实际的 SecretId 和 SecretKey
+	secretID := "ak"
+	secretKey := "sk"
+
+	// 配置
+	config := map[string]interface{}{"secretId": secretID, "secretKey": secretKey, "debug": false}
+
+	// 请求参数
+	params := map[string]interface{}{"Region": "gz", "Action": "GetFederationToken", "name": "alantong", "policy": "{\"statement\": [{\"action\": [\"name/cos:GetObject\",\"name/cos:PutObject\"],\"effect\": \"allow\",\"resource\":[\"qcs::cos:ap-guangzhou:uid/1253960454:prefix//1253960454/alangz/*\"]}],\"version\": \"2.0\"}"}
+
+	// 发送请求
+	retData, err := QcloudApi.SendRequest("sts", params, config)
+	if err != nil {
+		fmt.Print("Error.", err)
+		return
+	}
+	r := &Response{}
+	err = json.Unmarshal([]byte(retData), r)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	//获取临时ak、sk、token
+	tAk := r.Dat.Credentials.TmpSecretID
+	tSk := r.Dat.Credentials.TmpSecretKey
+	token := r.Dat.Credentials.SessionToken
+
+	u, _ := url.Parse("https://alangz-1253960454.cos.ap-guangzhou.myqcloud.com")
 	b := &cos.BaseURL{BucketURL: u}
 	c := cos.NewClient(b, &http.Client{
 		Transport: &cos.AuthorizationTransport{
-			SecretID: tmp_secId, 
-			SecretKey: tmp_secKey,
+			SecretID:     tAk,
+			SecretKey:    tSk,
 			SessionToken: token,
 			Transport: &debug.DebugRequestTransport{
 				RequestHeader:  true,
